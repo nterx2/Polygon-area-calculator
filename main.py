1a1e905c0755ef287bf9cda8e83f90148c24f28b
nTerx <nterx22@gmail.com>
2026-05-08 07:02:23 +0700

    Add get area, diagonal by using math sqrt  and change name method width and height.

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+import math
 class Rectangle:
     def __init__(self, width, height):
         self.width = width
         self.height = height
 
-    def get_width(self):
+    def set_width(self):
         return self.width
     
-    def get_height(self):
+    def set_height(self):
         return self.height
+    
+    def get_area(self):
+        return self.width * self.height
+    
+    def get_diagonal(self):
+        return math.sqrt(self.width ** 2 + self.height ** 2)
+    
